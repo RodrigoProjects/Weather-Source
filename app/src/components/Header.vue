0cf56d1f6f401e1c9998a64abaf561794ecd1db6
Utilizador <utilizador@pop-os.localdomain>
2021-08-31 01:59:14 +0100

    Header section has been changed for a more minimalist style

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center justify-center p-6 shadow-md w-full">
+  <div class="flex items-center justify-center p-6 border-b w-3/4">
     <div class="flex items-center justify-between w-48">
       <div class="flex flex-col items-center">
         <div class="header-title font-bold text-3xl opacity-80">Weather</div>
         <div class="header-title font-bold text-2xl opacity-70">Source</div>
       </div>
       <div>
-          <i class="fas fa-cloud-sun fa-3x opacity-90 bg-gradient-to-r from-yellow-400 text-transparent bg-clip-text"></i>
+          <i class="fas fa-cloud-sun fa-3x opacity-90 bg-gradient-to-br from-yellow-400 text-transparent bg-clip-text"></i>
       </div>
       
     </div>
